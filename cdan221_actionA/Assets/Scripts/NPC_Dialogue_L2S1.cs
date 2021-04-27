d2a1b78a123df27eaa49ae3443a0984c3f6248fd
sarahoneil <79662017+sarahoneil@users.noreply.github.com>
2021-04-27 17:49:41 -0400

    some more level 2 work

---
+++ cdan221_actionA/Assets/Scripts/NPC_Dialogue_L2S1.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC_Dialogue_L2S1 : MonoBehaviour {
+       //public Animator anim;
+       public GameObject dialogueBox;
+       public Text dialogueText;
+       public bool playerInRange = false;
+       public int primeInt = 0;
+	   public int startPoint = 1;
+
+       void Start () {
+              dialogueBox.SetActive(false);
+			  dialogueText.gameObject.SetActive(false);
+              //anim.SetBool("Chat", false)
+			  
+       }
+
+       void Update () {
+			
+			
+			if (Input.GetButtonDown("Talk") && playerInRange){ //can change teh key to
+                   if (dialogueBox.activeInHierarchy){
+                        //dialogueBox.SetActive(false);
+                        //anim.SetBool("Chat", false);
+
+                   } else {
+                        dialogueText.gameObject.SetActive(true);
+						dialogueText.text = "";
+                        dialogueBox.SetActive(true);
+                        //anim.SetBool("Chat", true);
+                   }
+				   
+            }
+			
+			
+			if (Input.GetButtonDown("Talk")){
+				NPCdialogue();
+			}
+			
+       }
+
+       public void NPCdialogue (){
+			if (primeInt == 0){
+				primeInt += startPoint;
+			} else {
+				primeInt +=1;
+			}
+
+			if (primeInt == 1){
+            dialogueText.text = "It's the stairs.";
+			}
+
+			if (primeInt ==2){
+            dialogueText.text =  "I just came from there.";
+			}
+			  
+			
+			if (primeInt == 3){
+            dialogueBox.SetActive(false);
+			dialogueText.gameObject.SetActive(false);
+			primeInt = 0;
+			}
+			
+			if (primeInt == 4){
+            dialogueText.text = "D-Doctor Claudia? Is that you?";
+			}
+			
+			if (primeInt == 5){
+            dialogueText.text = "Ahaha, no worries! I'm definitely hard at work in here! Just like always!";
+			}
+			
+			if (primeInt == 6){
+            dialogueText.text = "In fact, you could name any patient and I could tell you, like... Anything about them! That's how diligent I am!";
+			}
+			
+			if (primeInt == 7){
+            dialogueText.text = "Haha... ha...";
+			}
+			  
+			if (primeInt == 8){
+            dialogueBox.SetActive(false);
+			dialogueText.gameObject.SetActive(false);
+			primeInt = 0;
+			}
+			
+			if (primeInt == 12){
+            dialogueText.text = "Still no light.";
+			}
+			  
+			if (primeInt == 13){
+            dialogueBox.SetActive(false);
+			dialogueText.gameObject.SetActive(false);
+			primeInt = 0;
+			}
+			
+			if (primeInt == 14){
+            dialogueText.text = "Still no dice on getting that panel off. Do you really want to crawl in there?";
+			}
+			  
+			if (primeInt == 15){
+            dialogueBox.SetActive(false);
+			dialogueText.gameObject.SetActive(false);
+			primeInt = 0;
+			}
+		}
+
+       private void OnTriggerEnter2D(Collider2D other){
+             if (other.gameObject.tag == "Player") {
+                   playerInRange = true;
+                   primeInt = 0;
+				   Debug.Log("Hit Space to talk");
+                  }
+             }
+                        
+       private void OnTriggerExit2D(Collider2D other){
+             if (other.gameObject.tag == "Player") {
+                   playerInRange = false;
+                   dialogueBox.SetActive(false);
+				   dialogueText.gameObject.SetActive(false);
+                   //Debug.Log("Player left range");
+             }
+       }
+}
---
+++ cdan221_actionA/Assets/Scripts/NPC_Dialogue_L2S2.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC_Dialogue_L2S2 : MonoBehaviour {
+       //public Animator anim;
+       public GameObject dialogueBox;
+       public Text dialogueText;
+       public bool playerInRange = false;
+       public int primeInt = 0;
+	   public int startPoint = 1;
+
+       void Start () {
+              dialogueBox.SetActive(false);
+			  dialogueText.gameObject.SetActive(false);
+              //anim.SetBool("Chat", false)
+			  
+       }
+
+       void Update () {
+			
+			
+			if (Input.GetButtonDown("Talk") && playerInRange){ //can change teh key to
+                   if (dialogueBox.activeInHierarchy){
+                        //dialogueBox.SetActive(false);
+                        //anim.SetBool("Chat", false);
+
+                   } else {
+                        dialogueText.gameObject.SetActive(true);
+						dialogueText.text = "";
+                        dialogueBox.SetActive(true);
+                        //anim.SetBool("Chat", true);
+                   }
+				   
+            }
+			
+			
+			if (Input.GetButtonDown("Talk")){
+				NPCdialogue();
+			}
+			
+       }
+
+       public void NPCdialogue (){
+			if (primeInt == 0){
+				primeInt += startPoint;
+			} else {
+				primeInt +=1;
+			}
+
+			if (primeInt == 1){
+            dialogueText.text = "There are a bunch of folders here. I can't make out what any of them are.";
+			}
+
+			if (primeInt ==2){
+            dialogueText.text =  "They each have a labeled tab and some papers inside.";
+			}
+			  
+			
+			if (primeInt == 3){
+            dialogueBox.SetActive(false);
+			dialogueText.gameObject.SetActive(false);
+			primeInt = 0;
+			}
+			
+			if (primeInt == 8){
+            dialogueText.text = "Do you dare give the metal door of this vent a little tug?";
+			}
+			
+			if (primeInt == 9){
+            dialogueText.text = "...Oh my stars! Could it be? The panel's loose! Will you finally crawl in that dusty little maze?";
+			}
+			
+			if (primeInt == 10){
+            dialogueText.text = "No, absolutely not. You can feel around in there for good measure, though. Feels like more scratches!";
+			}
+			  
+			if (primeInt == 11){
+            dialogueBox.SetActive(false);
+			dialogueText.gameObject.SetActive(false);
+			primeInt = 0;
+			}
+			
+			if (primeInt == 12){
+            dialogueText.text = "Still no light.";
+			}
+			  
+			if (primeInt == 13){
+            dialogueBox.SetActive(false);
+			dialogueText.gameObject.SetActive(false);
+			primeInt = 0;
+			}
+			
+			if (primeInt == 14){
+            dialogueText.text = "Still no dice on getting that panel off. Do you really want to crawl in there?";
+			}
+			  
+			if (primeInt == 15){
+            dialogueBox.SetActive(false);
+			dialogueText.gameObject.SetActive(false);
+			primeInt = 0;
+			}
+		}
+
+       private void OnTriggerEnter2D(Collider2D other){
+             if (other.gameObject.tag == "Player") {
+                   playerInRange = true;
+                   primeInt = 0;
+				   Debug.Log("Hit Space to talk");
+                  }
+             }
+                        
+       private void OnTriggerExit2D(Collider2D other){
+             if (other.gameObject.tag == "Player") {
+                   playerInRange = false;
+                   dialogueBox.SetActive(false);
+				   dialogueText.gameObject.SetActive(false);
+                   //Debug.Log("Player left range");
+             }
+       }
+}
