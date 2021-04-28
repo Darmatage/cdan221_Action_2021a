0ca3c95ebfcc338ebea1105f01a186b70261b7e4
MichaelMauriello <mrmauriello@massart.edu>
2021-04-27 21:34:15 -0400

    added doctor door noise

---
+++ cdan221_actionA/Assets/Scripts/DoctorNoise.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoctorNoise : MonoBehaviour
+{
+	public GameObject DoctorNoiseSource;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        DoctorNoiseSource.SetActive(false);
+    }
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag == "Player"){
+			DoctorNoiseSource.SetActive(true);
+        }
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if (other.gameObject.tag == "Player"){
+			DoctorNoiseSource.SetActive(false);
+        }
+	}
+}
+
