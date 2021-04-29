702d04954b545a66637f06edc36b61107ca80f36
JasonWiser <JasonWiserArt@gmail.com>
2021-04-29 01:56:03 -0400

    Added Player Scale functionality, to show depth in scenes. Added touching walls

--- cdan221_actionA/Assets/Scripts/PlayerMoveAround.cs
+++ cdan221_actionA/Assets/Scripts/PlayerMoveAround.cs
@@ -4,16 +4,30 @@ using UnityEngine;
 
 public class PlayerMoveAround : MonoBehaviour {
 
-      public Animator animator;
-      public Rigidbody2D rb2D;
-      private bool FaceRight = true; // determine which way player is facing.
-      public static float runSpeed = 10f;
-      public float startSpeed = 10f;
-      public bool isAlive = true;
+    public Animator animator;
+    public Rigidbody2D rb2D;
+    private bool FaceRight = true; // determine which way player is facing.
+    public static float runSpeed = 10f;
+    public float startSpeed = 10f;
+    public bool isAlive = true;
+	  
+	public bool isNearWall = false;
+	  
+	//scaleByDepth
+	public bool scaleByDepth = false;
+	public float scaleMultiplier = 1.2f; //set to 1 for greater depth
+	private float scaleHeight = -5.6f;
+	private Vector3 startHeight;	
 
       void Start(){
            animator = gameObject.GetComponentInChildren<Animator>();
            rb2D = transform.GetComponent<Rigidbody2D>();
+		   
+		   //scaleByDepth
+		   if (GameObject.FindWithTag("PlayerScaleBottom") != null){
+			   scaleHeight = GameObject.FindWithTag("PlayerScaleBottom").transform.position.y;
+		   }
+		   startHeight = transform.localScale;
       }
 
       void Update(){
@@ -23,15 +37,48 @@ public class PlayerMoveAround : MonoBehaviour {
            if (isAlive == true){
                  
                 transform.position = transform.position + hvMove * runSpeed * Time.deltaTime;
-            
+
 
 				if ((Input.GetAxis("Horizontal") != 0) || (Input.GetAxis("Vertical") != 0)){
-					animator.SetBool ("Walk", true);
-				} else {animator.SetBool ("Walk", false);}
+					if (isNearWall == true){
+						animator.SetBool ("WalkWall", true);
+						animator.SetBool ("Walk", false);
+						animator.SetBool ("IdleWall", false);
+					} else if (isNearWall == false){
+						animator.SetBool ("Walk", true);
+						animator.SetBool ("WalkWall", false);
+						animator.SetBool ("IdleWall", false);
+					}
+				} else {
+					animator.SetBool ("WalkWall", false);
+					animator.SetBool ("Walk", false);
+					if (isNearWall == true){
+						animator.SetBool ("IdleWall", true);
+					} else if (isNearWall == false){
+						animator.SetBool ("IdleWall", false);
+					}
+				}
 
             // NOTE: if input is moving the Player right and Player faces left, turn, and vice-versa
 				if ((hvMove.x >0 && !FaceRight) || (hvMove.x <0 && FaceRight)){
 					playerTurn();
+				}
+				
+				//scaleByDepth:
+				if (scaleByDepth == true){
+					Transform sceneBottom= GameObject.FindWithTag("PlayerScaleBottom").transform;
+					float distanceFromBottom = Mathf.Abs(sceneBottom.position.y - transform.position.y);
+					//transform.localScale = startHeight * 4 * (1/distanceFromBottom);
+					if (FaceRight){ 
+						transform.localScale = startHeight * 4 * (scaleMultiplier/distanceFromBottom);					
+					} else {
+						Vector3 theScale = transform.localScale;
+						theScale.x = startHeight.x * -4 * (scaleMultiplier/distanceFromBottom);
+						theScale.y = startHeight.y * 4 * (scaleMultiplier/distanceFromBottom);
+						theScale.z = startHeight.z * 4 * (scaleMultiplier/distanceFromBottom);
+						transform.localScale = theScale;
+					}
+					Debug.Log("CurrentSize = " + transform.localScale);
 				}
 			}
       }
@@ -45,4 +92,20 @@ public class PlayerMoveAround : MonoBehaviour {
             theScale.x *= -1;
             transform.localScale = theScale;
       }
+	  
+	  void OnCollisionEnter2D(Collision2D other){
+            if (other.gameObject.tag == "sceneWall"){
+                  isNearWall = true;
+			} else {
+                  isNearWall = false;
+            }
+      }
+
+     void OnCollisionExit2D(Collision2D other){
+            if (other.gameObject.tag == "sceneWall"){
+                  isNearWall = false;
+			}
+      }
+	  
+	  
 }
