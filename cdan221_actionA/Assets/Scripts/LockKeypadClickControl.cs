6a88d2f558d64556bf913248c39842c3cfdde8c9
MichaelMauriello <mrmauriello@massart.edu>
2021-04-30 21:52:43 -0400

    Added Lock Keypad and Pause Menus to current scenes

---
+++ cdan221_actionA/Assets/Scripts/LockKeypadClickControl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LockKeypadClickControl : MonoBehaviour
+{
+	public static string correctCode = "J97EKF";
+	public static string playerCode = "";
+	
+	public static int totalDigits = 0;
+	
+	public string ReturnLevel = "Level1Scene2";
+	public string SolvedLevel = "Level2Scene1";
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.E)){
+			GoBack();
+        }
+		
+        Debug.Log(playerCode);
+		if (totalDigits == 6){
+			if (playerCode == correctCode){
+				Debug.Log("Correct!");
+				SolvedKeypad();
+			} else {
+				playerCode = "";
+				totalDigits = 0;
+				Debug.Log("INCORRECT.");
+			}
+		}
+    }
+	
+	void OnMouseUp(){
+		playerCode += gameObject.name;
+		totalDigits += 1;
+	}
+	
+	public void GoBack(){
+		SceneManager.LoadScene (ReturnLevel);
+	}
+	
+	public void SolvedKeypad(){
+		SceneManager.LoadScene (SolvedLevel);
+	}
+}
