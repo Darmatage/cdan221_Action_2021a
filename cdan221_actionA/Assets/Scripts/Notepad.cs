ceedf8cdcd0fb23a928c9623d4858c78cfaa6a0d
sarahoneil <79662017+sarahoneil@users.noreply.github.com>
2021-05-10 15:45:54 -0400

    notepad prefab added!
    it can't be turned on and off yet but it's functional!

---
+++ cdan221_actionA/Assets/Scripts/Notepad.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Notepad : MonoBehaviour {
+
+        public static string myNotes;
+        public GameObject inputField;
+        public GameObject textDisplay;
+
+        public void Start(){
+                UpdateNotes();
+        }
+
+        public void UpdateNotes(){
+                myNotes = myNotes + " " + inputField.GetComponentInChildren<Text>().text;
+                textDisplay.GetComponent<Text>().text = myNotes;
+                inputField.GetComponent<InputField>().text = "";
+        }
+}
